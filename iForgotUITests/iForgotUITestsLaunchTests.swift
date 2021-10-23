b6f77a6dfe1b72d812a14078b0c7e6571564e2d0
John <johnakpenyi@hotmail.com>
2021-10-23 18:21:37 +0100

    Initial Commit

---
+++ iForgotUITests/iForgotUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iForgotUITestsLaunchTests.swift
+//  iForgotUITests
+//
+//  Created by John Akpenyi on 23/10/2021.
+//
+
+import XCTest
+
+class iForgotUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
