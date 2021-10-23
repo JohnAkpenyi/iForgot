70ed9fb22dc54bf42ef99f708344728491cc3499
John <johnakpenyi@hotmail.com>
2021-10-23 23:53:05 +0100

    FSCalander testing

---
+++ Pods/Target Support Files/Pods-iForgot/Pods-iForgot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iForgot : NSObject
+@end
+@implementation PodsDummy_Pods_iForgot
+@end
---
+++ Pods/Target Support Files/Pods-iForgotTests/Pods-iForgotTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iForgotTests : NSObject
+@end
+@implementation PodsDummy_Pods_iForgotTests
+@end
